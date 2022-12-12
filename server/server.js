ff85d88c671e4962662584615976e1e61614ccbd
ejackson1228 <ejackson1228@gmail.com>
2022-12-12 18:02:36 -0500

    fixed typedef and resolvers bugs and set up server.js for graphQL API

--- server/server.js
+++ server/server.js
@@ -1,43 +1,43 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const path = require('path');
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
 
-const { typeDefs, resolvers } = require('./schemas');
-const { authMiddleware } = require('./utils/auth.js');
-const db = require('./config/connection');
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth.js");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 
-
 const app = express();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 //add apollo middleware here
-const serverStart = async () => {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware,
-  });
+const serverStart = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`)
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+    });
+  });
 };
 
-serverStart();
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
-
-
+serverStart(typeDefs, resolvers);
 
 // const routes = require('./routes');
 // app.use(routes);
